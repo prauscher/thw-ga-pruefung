045e79fe5e1a5f397d37db4182769b1f22c7545e
Patrick Rauscher <prauscher@prauscher.de>
2023-10-19 20:41:32 +0200

    begin with local state

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 import tornado
 import tornado.websocket
 
@@ -25,7 +27,25 @@ class BroadcastWebSocketHandler(tornado.websocket.WebSocketHandler):
 
 
 class MessageHandler(BroadcastWebSocketHandler):
+    state = {}
+    last_msg = 0
+
+    def open(self):
+        super().open()
+        self.write_message(json.dumps({"m":"init", "state": self.state, "no": self.last_msg}))
+
+    def process_station(self, index, name):
+        if "stations" not in self.state:
+            self.state["stations"] = {}
+        self.state["stations"][index] = name
+
     def on_message(self, message):
-        # TODO process command
-        self.broadcast(f"{self}: {message}")
-        print(self, message)
+        msg = json.loads(message)
+
+        handler = getattr(self, f"process_{msg.get('m','')}")
+        if handler:
+            handler(**msg)
+
+        self.last_msg += 1
+        self.broadcast(json.dumps({"no": self.last_msg, **msg}))
+        print(f"{datetime.now():%Y-%m-%d %H:%M:%S.%f} | {self.last_msg:04d} | {msg}")
